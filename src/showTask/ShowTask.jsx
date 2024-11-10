a26ed27c6e8ebd5b8f0b8d82089e100510ca69da
muhammedArsat <ars.code21@gmail.com>
2024-11-10 18:28:24 +0530

    search function implemented

--- src/showTask/ShowTask.jsx
+++ src/showTask/ShowTask.jsx
@@ -7,6 +7,23 @@ import { MdDelete } from "react-icons/md";
 
 function ShowTask() {
   const [tasks, setTasks] = useState([]);
+  const [search, setSearch] = useState("");
+
+
+
+  const handleSearch = () =>{
+
+    if(search.trim()===""){
+      getAllTasks();
+    }
+    else{
+      const filteredTask = tasks.filter(taks => 
+        taks.title.toLowerCase().includes(search.toLowerCase())
+      );
+      setTasks(filteredTask);
+    }
+
+  }
 
   const getAllTasks = async () => {
     const allTasks = await getDocs(collection(db, "tasks"));
@@ -19,34 +36,40 @@ function ShowTask() {
 
   const handleDelete = async (id) => {
     await deleteDoc(doc(db, "tasks", id));
-    getAllTasks(); // Refresh task list after deletion
+    getAllTasks(); 
   };
 
   return (
     <div>
       <NavBar />
+      <div className="search-container">
+        <input type="text" value={search} onChange={(e)=>{setSearch(e.target.value)}} placeholder="search Here..."/>
+        <button className="btn-search" onClick={handleSearch} > 
+          search
+        </button>
+      
+
+      </div>
 
       <div className="wrapper">
-        {tasks.length === 0 ? (
-          "No tasks"
-        ) : (
-          tasks.map((task, index) => {
-            return (
-              <div className="box" key={index}>
-                <div className="task-header">
-                  <h4>Title: {task.title}</h4>
-                  <span
-                    className="delete-icon"
-                    onClick={() => handleDelete(task.id)}
-                  >
-                    <MdDelete />
-                  </span>
+        {tasks.length === 0
+          ? "No tasks"
+          : tasks.map((task, index) => {
+              return (
+                <div className="box" key={index}>
+                  <div className="task-header">
+                    <h4>Title: {task.title}</h4>
+                    <span
+                      className="delete-icon"
+                      onClick={() => handleDelete(task.id)}
+                    >
+                      <MdDelete />
+                    </span>
+                  </div>
+                  <p>{task.content}</p>
                 </div>
-                <p>{task.content}</p>
-              </div>
-            );
-          })
-        )}
+              );
+            })}
       </div>
     </div>
   );
